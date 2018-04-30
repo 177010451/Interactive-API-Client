d95107a834629e92365efb8973ac4064d63200db
177010451 <177010451@stu.vtc.edu.hk>
2018-04-30 23:07:15 +0800

    4
    4

--- modules/dataSource.js
+++ modules/dataSource.js
@@ -4,7 +4,7 @@ var Server = require('mongodb').Server;
 var mongoClient = new MongoClient(new Server('ds263639.mlab.com', 63639));
 var dbName = 'nodejsweather';
 
-mongoClient.connect(function(err, mongoClient) {
+mongoClient.connect('177010451', 'Aa68151586', function(err, mongoClient) {
 	var db = mongoClient.db(dbName);
 	exports.db = db;
 });
