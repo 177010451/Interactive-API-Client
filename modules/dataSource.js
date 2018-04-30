22687c66e1ca7f67efe55101f7795dd75e2bdee2
177010451 <177010451@stu.vtc.edu.hk>
2018-04-30 22:44:33 +0800

    33
    33

--- modules/dataSource.js
+++ modules/dataSource.js
@@ -1,8 +1,8 @@
 /* init database */
 var MongoClient = require('mongodb').MongoClient;
 var Server = require('mongodb').Server;
-var mongoClient = new MongoClient(new Server('localhost', 27017));
-var dbName = 'Test';
+var mongoClient = new MongoClient(new Server('ds263639.mlab.com', 63639));
+var dbName = 'nodejsweather';
 
 mongoClient.connect(function(err, mongoClient) {
 	var db = mongoClient.db(dbName);
